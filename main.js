279cab3443c01f50ab5a08eab191a05556b950d6
JuliaRees <160231773+SabrinaRees@users.noreply.github.com>
2024-03-31 13:16:42 +0800

    ahead

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ lightGallery(document.getElementById('lightgallery'), {
     download: false
     
 });
+
+
+
+
+
+
+var allPanels = $('.accordion > .dd').hide();
+
+	$('.accordion > .dt > a').click(function() {
+		// allPanels.slideUp();
+
+        
+   // $(this).filter('.open').removeClass('open').addClass('closing')
+   // $('.accordion > .dt > a').removeClass('open');
+ // $(this).not(".closing").addClass('open').parent().next().slideDown();
+   // $('.accordion > .dt > a').removeClass('closing');
+    
+   $(this).toggleClass('open') 
+   $(this).parent().next().slideToggle() 
+
+   return false; 
+	});
